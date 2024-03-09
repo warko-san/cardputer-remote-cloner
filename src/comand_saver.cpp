d6dc21c10978e3910eb31ffb37de423f919a1434
Warko <swartalf222@gmail.com>
2024-03-10 00:31:28 +0200

    Rewrite file each time want to save controls

--- src/comand_saver.cpp
+++ src/comand_saver.cpp
@@ -14,56 +14,21 @@ void ComandSaver::copyKeySetup(ProcessHandler &processHandler, SDcard &sdCard, I
     switch (processHandler.getSavedSendProcess())
     {
     case Process::COPY_MAIN_CONTROLS:
-        if (!sdCard.exists((sdCard.rootDir + data + "/main_controls.bin").c_str()))
-        {
-            sdCard.createEmptyFile((sdCard.rootDir + data + "/main_controls.bin").c_str());
-        }
+        sdCard.createEmptyFile((sdCard.rootDir + data + "/main_controls.bin").c_str());
         break;
     case Process::COPY_NUMBERS:
-        if (!sdCard.exists((sdCard.rootDir + data + "/num_controls.bin").c_str()))
-        {
-            sdCard.createEmptyFile((sdCard.rootDir + data + "/num_controls.bin").c_str());
-        }
+        sdCard.createEmptyFile((sdCard.rootDir + data + "/num_controls.bin").c_str());
         break;
     case Process::COPY_NAVIGATION:
-        if (!sdCard.exists((sdCard.rootDir + data + "/nav_controls.bin").c_str()))
-        {
-            sdCard.createEmptyFile((sdCard.rootDir + data + "/nav_controls.bin").c_str());
-        }
+        sdCard.createEmptyFile((sdCard.rootDir + data + "/nav_controls.bin").c_str());
         break;
     case Process::COPY_MISC:
-        if (!sdCard.exists((sdCard.rootDir + data + "/misc_controls.bin").c_str()))
-        {
-            sdCard.createEmptyFile((sdCard.rootDir + data + "/misc_controls.bin").c_str());
-        }
+        sdCard.createEmptyFile((sdCard.rootDir + data + "/misc_controls.bin").c_str());
         break;
     }
     irHandler.readSetup();
     delay(200);
 }
-
-// void ComandSaver::copyKeyLoop(ScreenProvider &ScreenProvider, ProcessHandler &processHandler, SDcard &sdCard)
-// {
-//     menuHandler.copyKeyInternalLoop();
-
-//     if (irHandler.Decode())
-//     {
-//         printIRResultShort(&Serial, &currentData, false);
-
-//         if (currentData.protocol == UNKNOWN)
-//         {
-//             irHandler.Resume();
-//             return;
-//         }
-//         else
-//         {
-//             prevData = currentData;
-//         }
-
-//         delay(300);
-//         irHandler.Resume(); // resume receiver
-//     }
-// }
 
 void ComandSaver::saveCommand(IRData prevData, IRData currentData, ScreenProvider &screenProvider, ProcessHandler &processHandler, MenuHandler &menuHandler, SDcard &sdCard)
 {
@@ -75,7 +40,6 @@ void ComandSaver::saveCommand(IRData prevData, IRData currentData, ScreenProvide
             if (saveConfirmations == 1 && !comandSaved)
             {
                 sdCard.appendToFileIrData((sdCard.rootDir + data + "/main_controls.bin").c_str(), currentData);
-                //   menuHandler.drawmenu(mainCtrM, copy_main_size, MarkType::CHECK);
                 screenProvider.printCommandData(currentData.address, currentData.command, false, comandSaved);
                 comandSaved = true;
                 saveConfirmations++;
@@ -83,7 +47,6 @@ void ComandSaver::saveCommand(IRData prevData, IRData currentData, ScreenProvide
         }
         else
         {
-            //   menuHandler.drawmenu(mainCtrM, copy_main_size, MarkType::DOT);
             screenProvider.printCommandData(currentData.address, currentData.command, true, comandSaved);
             saveConfirmations++;
         }
@@ -94,7 +57,6 @@ void ComandSaver::saveCommand(IRData prevData, IRData currentData, ScreenProvide
             if (saveConfirmations == 1 && !comandSaved)
             {
                 sdCard.appendToFileIrData((sdCard.rootDir + data + "/num_controls.bin").c_str(), currentData);
-                //         menuHandler.drawmenu(btnCtrM, copy_num_size, MarkType::CHECK);
                 screenProvider.printCommandData(currentData.address, currentData.command, false, comandSaved);
                 comandSaved = true;
                 saveConfirmations++;
@@ -102,7 +64,6 @@ void ComandSaver::saveCommand(IRData prevData, IRData currentData, ScreenProvide
         }
         else
         {
-            //         menuHandler.drawmenu(btnCtrM, copy_num_size, MarkType::DOT);
             screenProvider.printCommandData(currentData.address, currentData.command, true, comandSaved);
             saveConfirmations++;
         }
@@ -114,14 +75,12 @@ void ComandSaver::saveCommand(IRData prevData, IRData currentData, ScreenProvide
             {
                 sdCard.appendToFileIrData((sdCard.rootDir + data + "/nav_controls.bin").c_str(), currentData);
                 screenProvider.printCommandData(currentData.address, currentData.command, false, comandSaved);
-                //           menuHandler.drawmenu(navCtrM, copy_nav_size, MarkType::CHECK);
                 comandSaved = true;
                 saveConfirmations++;
             }
         }
         else
         {
-            //       menuHandler.drawmenu(navCtrM, copy_nav_size, MarkType::DOT);
             screenProvider.printCommandData(currentData.address, currentData.command, true, comandSaved);
             saveConfirmations++;
         }
@@ -133,14 +92,12 @@ void ComandSaver::saveCommand(IRData prevData, IRData currentData, ScreenProvide
             {
                 sdCard.appendToFileIrData((sdCard.rootDir + data + "/misc_controls.bin").c_str(), currentData);
                 screenProvider.printCommandData(currentData.address, currentData.command, false, comandSaved);
-                //           menuHandler.drawmenu(miscCtrM, copy_misc_size, MarkType::CHECK);
                 comandSaved = true;
                 saveConfirmations++;
             }
         }
         else
         {
-            //           menuHandler.drawmenu(miscCtrM, copy_misc_size, MarkType::DOT);
             screenProvider.printCommandData(currentData.address, currentData.command, true, comandSaved);
             saveConfirmations++;
         }
